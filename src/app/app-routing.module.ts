9e0537549d267b048140bd60f9d41b317173e269
Rafael Araujo Chinaglia <chinaglia.rafa@gmail.com>
2022-09-26 21:55:54 -0300

    feat(ui): adiciona menu lateral e tela Sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  { path: '', component: HomePageComponent },
+  { path: 'calculator', component: HomeComponent },
+  { path: 'sobre', component: AboutComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Compiladores';
+
+  menu = [
+    { name: 'Início', icon: 'home', link: '/' },
+    { name: 'Calculadora', icon: 'calculate', link: '/calculator' },
+    { name: 'Sobre', icon: 'info', link: '/sobre' },
+  ];
 }
